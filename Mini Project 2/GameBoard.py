83b3c39d09c50aa0c9fc780324425d73013418a1
jihaneerraji025 <jihaneerraji17@gmail.com>
2026-03-05 10:43:32 +0000

    Mini Project 2: Checkers Game Complete human vs human version

--- Mini Project 2/GameBoard.py
+++ Mini Project 2/GameBoard.py
@@ -11,9 +11,10 @@ class GameBoard:
 
         self.SetupInitialBoard()
 
+
     def SetupInitialBoard(self):
 
-        # WHITE peices (B) ABOVE
+        # WHITE pieces (B) ABOVE
         self.board[0][1] = 'B'
         self.board[0][3] = 'B'
         self.board[0][5] = 'B'
@@ -29,7 +30,7 @@ class GameBoard:
         self.board[2][5] = 'B'
         self.board[2][7] = 'B'
 
-        #BLACK peices (B) LAST 3
+        #BLACK pieces (B) LAST 3
         self.board[5][0] = 'W'
         self.board[5][2] = 'W'
         self.board[5][4] = 'W'
@@ -46,12 +47,13 @@ class GameBoard:
         self.board[7][6] = 'W'
 
     def DisplayBoard(self):
-        print("  0 1 2 3 4 5 6 7") #prints teh number of the columns |
+        #print("  0 1 2 3 4 5 6 7") #prints teh number of the columns |
         for row in range(8):
-            print(f"{row}", end="")  # prints through iterations the number of teh rows -- ex: 0 does not go to teh next line
+            #print(f"{row}", end="")  # prints through iterations the number of teh rows -- ex: 0 does not go to teh next line
             for col in range (8):
                 print(f"{self.board[row][col]}", end="") #prints the content after all iteration B . B . B . B .
-            print ()
+            print ()   
+        print()
 
     def IsValidMove(self, start_row, start_col, target_row, target_col, player):
         #checking the bounderies
@@ -63,47 +65,95 @@ class GameBoard:
         piece = self.board[start_row][start_col]
         if piece not in [player, player.lower()]:  # 'W','w' or 'B','b'
             return False
-            
-        # target should be empty
-        if self.board[target_row][target_col] != '.':
-            return False
-        
-        #dioganal move ( 1 square only)
+
         row_diff = target_row - start_row
         col_diff = target_col - start_col
-        
-        if (abs(row_diff) != 1 or abs(col_diff) != 1) and (piece == 'W' or piece =='B'):
-            return False
-        
+
         #WHITE should move up ( to smaller numbers) unless it is a king
         if player == 'W':
             if row_diff > 0 and piece == 'W':  #  BLACK can't move down (not king) -- we go from 0 down to 7 that is why the difference is positive
                 return False
             
-        
         else: #BLACK should move down ( to bigger numbers) unless it is a king
             if row_diff < 0 and piece == 'B':  #BLACK can't move up (to smalle numbers)
+                return False            
+
+        #dioganal move, even king move the same number
+     
+        if abs(row_diff) not in (1,2) or abs(col_diff) not in  (1,2) or abs(row_diff) != abs(col_diff): #1 simple / 2 jump
+            return False
+        
+        #simple move
+        if abs(row_diff) == 1:
+            if self.board[target_row][target_col] != '.': #target must be empty
                 return False
-        return True 
+            return True
+        
+        #when we having a jump
+        if abs(row_diff) == 2: 
+            if self.board[target_row][target_col] != '.': #target empty for landing
+                return False
+            #enemy in the middle
+            mid_row = (start_row + target_row) // 2
+            mid_col = (start_col + target_col) // 2
+            mid_piece = self.board[mid_row][mid_col]
+            if mid_piece == '.' or mid_piece.upper() == player.upper(): #must be opponent 
+                return False
+            return True
+
+    def HasJump (self,player): # checking if it has any opponent piece to jump over and eat ------------ can be in the ui 
+        for row in range(8):
+            for col in range(8):
+                if self.board[row][col] in [player, player.lower()]: #the players piece
+                    for jump_row, jump_col in [(-2,-2), (-2,2), (2,-2),(2,2)]:
+                        new_row = row + jump_row
+                        new_col = col + jump_col
+
+                        if 0 <= new_row < 8 and 0 <= new_col < 8 and self.board[new_row][new_col] == '.':
+                            #check for anemy in the middle 
+                            middle_row = (row + new_row)//2
+                            middle_col = (col + new_col)//2
+                            enemy_there = self.board[middle_row][middle_col]
+                            
+                            if enemy_there != '.'and enemy_there.upper() != player.upper():
+                                return True
+        return False
+                            
 
     def MakeMove(self, start_row, start_col, target_row, target_col, player):
         if not self.IsValidMove(start_row, start_col, target_row, target_col, player):
             print("Invalid Move")
             return False
-        else:
-            piece = self.board[start_row][start_col] 
-            self.board[start_row][start_col] = '.' #removing the peice and placing
-            self.board[target_row][target_col] = piece #placing the peice in teh target 
+        
+          
+        piece = self.board[start_row][start_col] 
 
-            #KING PROMOTIION IF IT REACHES THE OTHER END 
-            if piece == 'W' and target_row == 0:
-                self.board[target_row][target_col]='w' #SMALL w IS THE KING
-            elif piece == 'B' and target_row == 7:
-                self.board[target_row][target_col]='b' #SMALL b IS THE KING
+        self.board[start_row][start_col] = '.'
+        self.board[target_row][target_col] = piece
+
+        #handle jump
+        if abs(target_row - start_row) == 2:
+            mid_row = (start_row + target_row) // 2
+            mid_col = (start_col + target_col) // 2
+            self.board[mid_row][mid_col] = '.'
+                
+                
+
+        #KING PROMOTIION IF IT REACHES THE OTHER END 
+        if piece == 'W' and target_row == 0:
+            self.board[target_row][target_col]='w' #SMALL w IS THE KING
+        elif piece == 'B' and target_row == 7:
+            self.board[target_row][target_col]='b' #SMALL b IS THE KING
             
-            print(f"SUCCESS: ({start_row},{start_col}) -> ({target_row},{target_col})")
-            self.DisplayBoard()
-            return True
+        print(f"SUCCESS: ({start_row},{start_col}) -> ({target_row},{target_col})")
+        self.DisplayBoard()
+        return True
+        
+# 2 moves - eat
+#
+
+gameboard = GameBoard()
+gameboard.DisplayBoard()
 
 
 
--- Mini Project 2/PlayingTheGame.py
+++ Mini Project 2/PlayingTheGame.py
@@ -1,20 +1,104 @@
-import GameBoard
+from GameBoard import GameBoard
 
-class playingTheGame:
+class PlayingTheGame:
     """
-    handles complete workflow between the white (human) and  the black (bot)
+    2 humans playing agaust each other, the bot will be added later
     """
 
     def __init__(self):
         self.gameBoard = GameBoard()
         self.moveNumber = 0 # to track how many moves happened 
+        self.currentPlayer = 'W' #the white player is the first one to start
 
-        #specified starting potion by the Human
-        self.MoveStartingLocationRow = 0
-        self.MoveStartingLocationCol = 0
+        #specified starting position by the Human
+        self.StartingMoveLocationRow = 0
+        self.StartingMoveLocationCol = 0
 
-        #specified target potion by the Human
+        #specified target position by the Human
         self.TargetingMoveLocationRow = 0
         self.TargetingMoveLocationCol = 0
+    
+    def SwitchPlayer(self):
+        if self.currentPlayer == 'W':
+            self.currentPlayer = 'B'
+        else:
+            self.currentPlayer = 'W'
 
-    def getStartingMoveLocation (self): #from the human whenerver it is his turn
+    def GetMoveLocation (self): #get the move from any player
+
+        while True:
+            try:
+                start_input = input(f'Player {self.currentPlayer} Move # {self.moveNumber+1} - starting location (row col): ').strip()
+                row, col = map(int, start_input.split())
+                self.StartingMoveLocationRow = row
+                self.StartingMoveLocationCol = col
+                break
+                
+            except ValueError:
+                print("ERROR: Please enter two numbers: For example [6 1]")
+
+        while True:
+            try:
+                target_input = input(f'Enter Target Location (row col): ').strip()
+                row, col = map(int, target_input.split())
+                self.TargetingMoveLocationRow  =  row
+                self.TargetingMoveLocationCol = col
+                break
+            except ValueError:
+                print("ERROR: Please enter two numbers: For example [6 1]")
+       
+    def IsGameOver (self):
+        white_count = 0
+        black_count = 0
+        for row in self.gameBoard.board:
+            for cell in row:
+                if cell.upper() == 'W':
+                    white_count +=1
+                elif cell.upper() =='B':
+                    black_count +=1
+        
+        if white_count ==0:
+            print(f"Black wins after {self.moveNumber} moves")
+            return True
+        if black_count == 0:
+            print(f"white wins after {self.moveNumber} moves")
+            return True
+        return False
+
+        
+
+    def PlayHumanTurn(self):
+        print(f"\n{'='*50}")
+        print(f"player {self.currentPlayer} Turn # {self.moveNumber+1}")
+
+        self.gameBoard.DisplayBoard()
+
+        self.GetMoveLocation()
+
+        if self.gameBoard.MakeMove(self.StartingMoveLocationRow, self.StartingMoveLocationCol, self.TargetingMoveLocationRow, self.TargetingMoveLocationCol, self.currentPlayer):
+            self.moveNumber+=1
+            print(f"Move {self.moveNumber} successful")
+        else:
+            print("Try again")
+            self.PlayHumanTurn()
+        
+    def PlayGameTwoPlayers(self):
+        while True:
+            self.PlayHumanTurn()
+            if self.IsGameOver():
+                break
+            self.SwitchPlayer()
+    
+        print("GAME OVER!")
+        self.gameBoard.DisplayBoard()
+
+if __name__ == "__main__":
+    game = PlayingTheGame()
+    game.PlayGameTwoPlayers()   
+
+
+
+    
+
+
+#check final
